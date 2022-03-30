7ea5d22ea8b9283eaf9a1661a4c6b876f6ce9fe5
Natalia Betancourt Millan <1108568867@u.icesi.edu.co>
2022-03-30 08:19:59 -0500

    DOM Exercise

--- js/shop.js
+++ js/shop.js
@@ -13,70 +13,70 @@ const products = [
         name: "THE FAMOUS AIRBRUSH FLAWLESS ROUTINE FACE KIT",
         price: 118,
         image: '../images/face_kit.png',
-        amount: 10,
+        stock: 10,
         color: "Beige",
     },
     {
         name: "SCIENCE-POWERED MAGIC CREAM LIGHT & SERUM KIT",
         price: 154,
         image: '../images/serum_kit.png',
-        amount: 20,
+        stock: 20,
         color: "Standard",
     },
     {
         name: "PILLOW TALK LIP KIT",
         price: 48,
         image: '../images/serum_kit.png',
-        amount: 15,
+        stock: 15,
         color: "Pillow Talk",
     }, 
     {
         name: "AIRBRUSH BRONZER",
         price: 50,
         image: '../images/bronzer.png',
-        amount: 0,
+        stock: 0,
         color: "Dark Carmel",
     },
     {
         name: "MATTE REVOLUTION",
         price: 32,
         image: '../images/matte_lipstick.png',
-        amount: 0,
+        stock: 0,
         color: "Pillow Talk Original",
     },
     {
         name: "THE HOLLYWOOD CONTOUR DUO",
         price: 62,
         image: '../images/hollywood_duo.png',
-        amount: 8,
+        stock: 8,
         color: "Classic",
     },
     {
         name: "AIRBRUSH FLAWLESS FOUNDATION",
         price: 40,
         image: '../images/foundation1.jpg',
-        amount: 4,
+        stock: 4,
         color: "Cool",
     },
     {
         name: "PILLOW TALK PUSH UP LASHES! MASCARA",
         price: 29,
         image: '../images/mascara.png',
-        amount: 0,
+        stock: 0,
         color: "Black",
     },
     {
         name: "LUXURY PALETTE",
         price: 50,
         image: '../images/eyeshadow.png',
-        amount: 30,
+        stock: 30,
         color: "Walk of No Shame",
     },
     {
         name: "MAGIC AWAY",
         price: 29,
         image: '../images/concealer.png',
-        amount: 0,
+        stock: 0,
         color: "Deep",
     }
 ]
@@ -88,7 +88,7 @@ function filter() {
     //Filter products for prices over 50 or if they're available in stock
     console.log("The products that cost more than 50$ or that are in stock are: ");
     products.forEach(product => {
-        if (product.price > 50 || product.amount > 0) {
+        if (product.price > 50 || product.stock > 0) {
             console.log(product.name);
         }
     });
@@ -107,12 +107,12 @@ function shopProducts(){
 function render(product){
          //Container
          const item = document.createElement("div");
-         const inStock = product.amount > 0 ? "IN STOCK" : "NOT IN STOCK"
+         const inStock = product.stock > 0 ? "IN STOCK" : "NOT IN STOCK"
          item.className = "shop__item";
          item.innerHTML = `
             <img class="shop__img" src="${product.image}">
             <h3 class="shop__product serif">${product.name}</h3>
-            <h5 class="sans">${"$" +product.price}</h5>
+            <h5 class="sans">${"$" + product.price}</h5>
             <p class="sans">${inStock}</p>
             <p class="sans">${"Color: " + product.color}</p>
          `;
