ad4f884f3176dfb9eb521d0d655a62457b9cf63f
Natalia Betancourt <nataliabetancourtm@gmail.com>
2022-04-21 18:02:09 -0500

    Login design made

---
+++ js/login.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDceSUOyydDIZvwKKWO189W_HaCHaN1Utw",
+  authDomain: "web-charlotte-shop.firebaseapp.com",
+  projectId: "web-charlotte-shop",
+  storageBucket: "web-charlotte-shop.appspot.com",
+  messagingSenderId: "726419666073",
+  appId: "1:726419666073:web:f982b8c85cbfc136cf7408"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
